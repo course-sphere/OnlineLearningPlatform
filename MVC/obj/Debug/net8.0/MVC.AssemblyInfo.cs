2e68402678c5d54b65df07a142441224966859df
TaHoang715 <taminhhoang.nk@gmail.com>
2026-01-20 17:15:19 +0700

    Add Login page

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b16a7a03a3ed4cf23d970cc8d15b5aba7621f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af43fcd2f1962ef6e4f7a72d3773a45f811e149e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
