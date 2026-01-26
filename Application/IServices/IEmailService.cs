9e259dfe3dbdd8edd171b46b3576ec2f5195cff8
namdang-fdp <namdangcoder@gmail.com>
2026-01-27 00:26:50 +0700

    feat: service to support for send reject email

--- Application/IServices/IEmailService.cs
+++ Application/IServices/IEmailService.cs
@@ -4,10 +4,8 @@ namespace Application.IServices
 {
     public interface IEmailService
     {
-        Task<ApiResponse> SendRejectCourseEmail(
-    string receiverName,
-    string receiverEmail,
-    string rejectReason,
-    string courseTitle);
+        Task<ApiResponse> SendRejectCourseEmail(string receiverName, string receiverEmail, string rejectReason, string courseTitle);
+
+        Task<ApiResponse> SendApproveCourseEmail(string receiverName, string receiverEmail, string courseTitle);
     }
 }
