c65273fd0c2e00050b4640e561a83feba86fe523
TaHoang715 <taminhhoang.nk@gmail.com>
2026-01-26 17:09:41 +0700

    update controller

--- MVC/Controllers/AdminController.cs
+++ MVC/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         //https://localhost:7276/Admin
